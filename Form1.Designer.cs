dc339cc5b5b6e5d7b187af5550600a808600c432
Matthue <matthewjury2@gmail.com>
2015-10-18 01:34:31 -0400

    Added the Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿using System;
+
+namespace AxeSpammerV4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.assholeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multipleStringsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assfucksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.niceAssToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keepGoingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trustMeHereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itsWorthItToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keepGoinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cLICKHEREToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assholeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(336, 238);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(81, 62);
+            this.start.TabIndex = 0;
+            this.start.Text = "Go!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(304, 235);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.assholeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 26);
+            // 
+            // assholeToolStripMenuItem
+            // 
+            this.assholeToolStripMenuItem.Name = "assholeToolStripMenuItem";
+            this.assholeToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.assholeToolStripMenuItem.Text = "asshole??";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.assfucksToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(444, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.multipleStringsToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // multipleStringsToolStripMenuItem
+            // 
+            this.multipleStringsToolStripMenuItem.Name = "multipleStringsToolStripMenuItem";
+            this.multipleStringsToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.multipleStringsToolStripMenuItem.Text = "Multiple Strings";
+            // 
+            // assfucksToolStripMenuItem
+            // 
+            this.assfucksToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.niceAssToolStripMenuItem,
+            this.assholeToolStripMenuItem1});
+            this.assfucksToolStripMenuItem.Name = "assfucksToolStripMenuItem";
+            this.assfucksToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.assfucksToolStripMenuItem.Text = "Assfucks";
+            // 
+            // niceAssToolStripMenuItem
+            // 
+            this.niceAssToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.keepGoingToolStripMenuItem});
+            this.niceAssToolStripMenuItem.Name = "niceAssToolStripMenuItem";
+            this.niceAssToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.niceAssToolStripMenuItem.Text = "Nice Ass";
+            // 
+            // keepGoingToolStripMenuItem
+            // 
+            this.keepGoingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmonToolStripMenuItem});
+            this.keepGoingToolStripMenuItem.Name = "keepGoingToolStripMenuItem";
+            this.keepGoingToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.keepGoingToolStripMenuItem.Text = "Keep Going";
+            // 
+            // cmonToolStripMenuItem
+            // 
+            this.cmonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trustMeHereToolStripMenuItem});
+            this.cmonToolStripMenuItem.Name = "cmonToolStripMenuItem";
+            this.cmonToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.cmonToolStripMenuItem.Text = "Cmon ";
+            // 
+            // trustMeHereToolStripMenuItem
+            // 
+            this.trustMeHereToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itsWorthItToolStripMenuItem});
+            this.trustMeHereToolStripMenuItem.Name = "trustMeHereToolStripMenuItem";
+            this.trustMeHereToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.trustMeHereToolStripMenuItem.Text = "Trust me here";
+            // 
+            // itsWorthItToolStripMenuItem
+            // 
+            this.itsWorthItToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.keepGoinToolStripMenuItem});
+            this.itsWorthItToolStripMenuItem.Name = "itsWorthItToolStripMenuItem";
+            this.itsWorthItToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.itsWorthItToolStripMenuItem.Text = "It\'s worth it";
+            // 
+            // keepGoinToolStripMenuItem
+            // 
+            this.keepGoinToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cLICKHEREToolStripMenuItem});
+            this.keepGoinToolStripMenuItem.Name = "keepGoinToolStripMenuItem";
+            this.keepGoinToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.keepGoinToolStripMenuItem.Text = "keep goin";
+            // 
+            // cLICKHEREToolStripMenuItem
+            // 
+            this.cLICKHEREToolStripMenuItem.Name = "cLICKHEREToolStripMenuItem";
+            this.cLICKHEREToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.cLICKHEREToolStripMenuItem.Text = "CLICK HERE!!!";
+            this.cLICKHEREToolStripMenuItem.Click += new System.EventHandler(this.cLICKHEREToolStripMenuItem_Click_1);
+            // 
+            // assholeToolStripMenuItem1
+            // 
+            this.assholeToolStripMenuItem1.Name = "assholeToolStripMenuItem1";
+            this.assholeToolStripMenuItem1.Size = new System.Drawing.Size(119, 22);
+            this.assholeToolStripMenuItem1.Text = "Anal?";
+            this.assholeToolStripMenuItem1.Click += new System.EventHandler(this.assholeToolStripMenuItem1_Click_1);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(336, 170);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(81, 62);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "Stop!";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Delay(ms):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(336, 83);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(444, 342);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AxeSpammer Version 4.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem assholeToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multipleStringsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assfucksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem niceAssToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keepGoingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cmonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trustMeHereToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itsWorthItToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keepGoinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cLICKHEREToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assholeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        internal System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AxeSpammerV4
+{
+    public partial class Form1 : Form
+    {
+        Thread SpamIt;
+        int delay;
+        string text;
+        int l = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SpamIt = new Thread(spam);
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void assholeToolStripMenuItem1_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("That's gross.", "Gross");
+        //}
+
+        //private void cLICKHEREToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("ass");
+        //}
+
+        private void assholeToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("That's gross.", "Gross");
+        }
+
+        private void cLICKHEREToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("ass");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            text = richTextBox1.Text;
+            l++;
+            richTextBox1.Enabled = false;
+            if(l == 1)
+            {
+                SpamIt.Start();
+            }
+
+            
+            if(l >= 2)
+            {
+                SpamIt.Resume();
+            }
+            start.Enabled = false;
+            textBox1.Enabled = false;
+        }
+        /// <summary>
+        /// This is the good stuff right here, the main part of the app!
+        /// </summary>
+        private void spam()
+        {
+                     
+        delay = Convert.ToInt32(textBox1.Text);
+            while (true)
+            {      
+                SendKeys.SendWait(text);
+                Thread.Sleep(delay);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = true;
+            start.Enabled = true;
+            textBox1.Enabled = true;
+
+
+            if (SpamIt.IsAlive == true)
+            {
+                SpamIt.Suspend();
+            }
+            if (SpamIt.IsAlive == false)
+            {
+                MessageBox.Show("Nothing to stop!", "YOU MORON",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Sure you want to quit? Stop all threads first!", "Quit?", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (dialog == DialogResult.Yes)
+            {
+                if (SpamIt.IsAlive == false)
+                {
+                    e.Cancel = false;
+                }
+                if (SpamIt.IsAlive == true)
+                {
+                    SpamIt.Resume();
+                    SpamIt.Abort();
+                    e.Cancel = false;
+                }
+            }
+            if (dialog == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
